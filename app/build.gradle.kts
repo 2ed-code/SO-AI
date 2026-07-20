f427ccd3672e7c6c9949b619a4a13462e55d5601
2ed-code <youssef.ofc.io@gmail.com>
2026-07-20 22:29:09 +0300

    Fix Compose plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -18,24 +19,16 @@ android {
     buildFeatures {
         compose = true
     }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
 }
 
 dependencies {
-
     implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation(
-        platform(
-            "androidx.compose:compose-bom:2024.12.01"
-        )
+        platform("androidx.compose:compose-bom:2024.12.01")
     )
 
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-
 }
