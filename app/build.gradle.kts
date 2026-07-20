071ad12c685d5d7b58ad6c7f070a882f199996c0
2ed-code <youssef.ofc.io@gmail.com>
2026-07-20 22:39:50 +0300

    Use Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,16 +19,23 @@ android {
     buildFeatures {
         compose = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
 
-    implementation(
-        platform("androidx.compose:compose-bom:2024.12.01")
-    )
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
 
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
 }
